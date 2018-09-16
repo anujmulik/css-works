e735cd10e644277934cc2b92d2420cbed5eb81ca
Anuj Mulik <anuj9306@gmail.com>
2018-09-16 12:25:36 +0530

    initializing angular for natours project

---
+++ natours/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
